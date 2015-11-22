d7095f86c53e1886b3116ef90ab9e17639784aeb
Shrihari <hari@hari.com>
2015-11-21 19:45:35 -0800

    Home page is ready

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def get
+    @response = 'This is a place holder'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # get 'meow/get'
+  get '/' => 'home#get'
   get 'meow/' => 'meow#get'
 
 
